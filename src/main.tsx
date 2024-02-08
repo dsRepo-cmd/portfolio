28960cd13214999f06c6cddfb366e6d21d329df7
ValpSerg <Ghostnord@gmail.com>
2024-02-08 11:42:25 +0200

    init

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AOS from "aos";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+AOS.init({
+  offset: 0,
+  duration: 1000,
+});
