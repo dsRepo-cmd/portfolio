8aadfecd6f6a4ee0b81c206b9c4ec726f77f0fc3
ValpSerg <Ghostnord@gmail.com>
2024-08-29 11:59:48 +0300

    Upd / resume

--- src/data/works.ts
+++ src/data/works.ts
@@ -15,7 +15,7 @@ export const projects: IProject[] = [
 
     link: "https://avion-app-seven.vercel.app",
     git: "https://github.com/dsRepo-cmd/avion-app",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nam nisi dolores ex ullam temporibus, sequi, totam assumenda veniam animi cupiditate eius inventore corporis perspiciatis aliquam tempore voluptates iure",
+    desc: "Built a responsive e-commerce platform for furniture with user registration and authentication. Integrated NextAuth for secure authentication, Mongoose for data management, and Tailwind CSS for UI. Added unit and integration tests with Jest.",
   },
 
   {
@@ -24,7 +24,7 @@ export const projects: IProject[] = [
     title: "Personal Trainer",
     link: "https://personal-trainers.netlify.app/",
     git: "https://github.com/dsRepo-cmd/personal-trainer",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae nam nisi dolores ex ullam temporibus, sequi, totam assumenda veniam animi cupiditate eius inventore corporis perspiciatis aliquam tempore voluptates iure",
+    desc: "Created a responsive site for personal training classes with dynamic layouts using isotope-layout, interactive animations with framer-motion, and a responsive carousel with react-slick. Styled with Tailwind CSS and clsx, and included Formspree for class registration.",
   },
   {
     id: 2,
