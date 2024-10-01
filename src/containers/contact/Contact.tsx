33d5d4d7ba48f070f8cd4355e8aacab819c8b7ed
ValpSerg <Ghostnord@gmail.com>
2024-10-01 14:39:16 +0300

    upd / fix

--- src/containers/contact/Contact.tsx
+++ src/containers/contact/Contact.tsx
@@ -24,7 +24,7 @@ const Contact = () => {
               data-aos="zoom-in"
               className=" text-yellow-500 font-extrabold text-3xl p-2 rounded-lg  shadow-[0_0_10px_1px_rgba(0,0,0,0.1)] dark:shadow-[0_0_10px_1px_rgba(255,255,255,0.1)] "
             >
-              <BsArrowRight className=" md:rotate-90" />
+              <BsArrowRight className=" sm:rotate-90" />
             </button>
           </div>
         </div>
