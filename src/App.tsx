e818f67ee82bd4f8c622a50074af25032b1f5a40
ValpSerg <Ghostnord@gmail.com>
2024-10-21 13:42:55 +0300

    Add / font change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { lazy, Suspense } from "react";
+import { useTranslation } from "react-i18next";
 import { BarLoader } from "react-spinners";
 export const Home = lazy(() => import("./pages/Home"));
 
 const App = () => {
+  const { i18n } = useTranslation();
+  const currentLanguage = i18n.language;
+  console.log(currentLanguage);
+
+  const fontClass = currentLanguage === "ua" ? "font_ua" : "font_en";
+
   return (
-    <div className="bg-white dark:bg-[#282c34] dark:text-white">
+    <div className={`bg-white dark:bg-[#282c34] dark:text-white ${fontClass}`}>
       <Suspense
         fallback={
           <div className="h-screen w-full flex items-center justify-center bg-[#282c34]">
